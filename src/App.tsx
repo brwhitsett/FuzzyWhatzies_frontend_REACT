42df84f97b04fb341306064018cb4029d37f4b1b
Steve Anderson <swanderson79@gmail.com>
2022-04-11 16:01:38 -0400

    quick fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Routes,
 } from "react-router-dom";
 import Header from "./components/Header";
+import HowTo from "./components/HowTo";
 import LeaderBoardRoute from "./components/LeaderBoardRoute";
 import MainRoute from "./components/MainRoute";
 import NewGameRoute from "./components/NewGameRoute";
@@ -15,7 +16,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/" element={<MainRoute />} />
           <Route path="/game" element={<NewGameRoute />} />
+          <Route path="/howto" element={<HowTo />} />
           <Route path="/leaderboard" element={<LeaderBoardRoute />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
