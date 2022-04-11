ef4f9a0193e3610df1409abac3a212e143f3299f
Dusan <dmomcilo@hotmail.com>
2022-04-11 15:35:37 -0400

    StatsLeaderboardRouterspluscomponents

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Routes,
 } from "react-router-dom";
 import Header from "./components/Header";
+import LeaderBoardRoute from "./components/LeaderBoardRoute";
 import MainRoute from "./components/MainRoute";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<MainRoute />} />
-
+          <Route path="/leaderboard" element={<LeaderBoardRoute />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
