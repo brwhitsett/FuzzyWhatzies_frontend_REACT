d01b1a533663c99dc42c27704e5ffed73006f7c2
brwhitsett <97193372+brwhitsett@users.noreply.github.com>
2022-04-14 13:37:28 -0400

    header and most of how to css

--- src/components/HowToRoute.tsx
+++ src/components/HowToRoute.tsx
@@ -5,20 +5,26 @@ const HowToRoute = () => {
   const navigate = useNavigate();
   return (
     <div className="HowToRoute">
-      <button onClick={() => navigate("/")}>Back</button>
-      <h2>How to Play:</h2>
-      <h3>The Goal:</h3>
-      <p>
-        We'll show you a Fuzzy (or not so Fuzzy) Whatzie. Answer all of the
-        categories within the selected time limit to score points.
-      </p>
-      <h3>Difficulty:</h3>
-      <p>Choose which categories to complete within the selected time frame.</p>
-      <h3>Speed:</h3>
-      <p>
-        Go at your own pace in the unlimited mode or crank up the speed to
-        really challenge yourself!
-      </p>
+      <button className="back-btn" onClick={() => navigate("/")}>
+        Back
+      </button>
+      <div className="main">
+        <h2 className="how-to">How to Play:</h2>
+        <h3>The Goal:</h3>
+        <p>
+          We'll show you a Fuzzy (or not so Fuzzy) Whatzie. Answer all of the
+          categories within the selected time limit to score points.
+        </p>
+        <h3>Difficulty:</h3>
+        <p>
+          Choose which categories to complete within the selected time frame.
+        </p>
+        <h3>Speed:</h3>
+        <p>
+          Go at your own pace in the unlimited mode or crank up the speed to
+          really challenge yourself!
+        </p>
+      </div>
     </div>
   );
 };
