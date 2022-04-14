62f7a78e4ba24fd61beea258ce65a73700d7b32b
brwhitsett <97193372+brwhitsett@users.noreply.github.com>
2022-04-14 05:04:59 -0400

    styling for header

---
+++ src/components/HowTo.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import "./HowTo.css";
+
+const HowTo = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="HowTo">
+      <button onClick={() => navigate("/")}>Back</button>
+      <h2>How to Play:</h2>
+      <h3>The Goal:</h3>
+      <p>
+        We'll show you a Fuzzy (or not so Fuzzy) Whatzie. Answer all of the
+        categories within the selected time limit to score points.
+      </p>
+      <h3>Difficulty:</h3>
+      <p>Choose which categories to complete within the selected time frame.</p>
+      <h3>Speed:</h3>
+      <p>
+        Go at your own pace in the unlimited mode or crank up the speed to
+        really challenge yourself!
+      </p>
+    </div>
+  );
+};
+
+export default HowTo;
