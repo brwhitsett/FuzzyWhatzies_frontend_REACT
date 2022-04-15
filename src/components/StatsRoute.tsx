2c381f200081ea67c4bec8b0cd2bb15a83385694
Dusan <dmomcilo@hotmail.com>
2022-04-15 16:27:40 -0400

    css statsroute

--- src/components/StatsRoute.tsx
+++ src/components/StatsRoute.tsx
@@ -2,13 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import Session from "../models/Session";
+import User from "../models/User";
 import { getSessionData } from "../services/SessionServices";
+import { getSingleUserData } from "../services/UserServices";
 import "./StatsRoute.css";
 
 const StatsRoute = () => {
   const navigate = useNavigate();
   const [level, setLevel] = useState("");
   const [userSessions, setUserSessions] = useState<Session[]>([]);
+  const [userScore, setUserScore] = useState<User>();
   const { user } = useContext(AuthContext);
 
   const getAndSetUserSessions = async () => {
@@ -17,8 +20,16 @@ const StatsRoute = () => {
     });
   };
 
+  const getAndSetUserScore = async () => {
+    getSingleUserData(user?.uid!).then((response) => {
+      setUserScore(response);
+      console.log(response);
+    });
+  };
+
   useEffect(() => {
     getAndSetUserSessions().then(() => {});
+    getAndSetUserScore().then(() => {});
   }, []);
 
   return (
@@ -43,57 +54,132 @@ const StatsRoute = () => {
         <option value="Total">Total</option>
       </select>
 
-      <h2 className="sessions">Last 3 Sessions</h2>
       {user ? (
-        <ol className="main-area">
-          {level === "Total" || level === ""
-            ? user &&
-              userSessions
-                .map(
-                  (userSession) =>
-                    user.displayName === userSession.displayName && (
-                      <li className="session-list">
-                        <p>Name: {userSession.displayName}</p>
-                        <p>Difficulty: {userSession.difficulty}</p>
-                        <p>Speed: {userSession.speed}</p>
-                        <p>Correct: {userSession.correct}</p>
-                        <p>
-                          Percent Correct:
-                          {`${(
-                            (userSession.correct / userSession.total) *
-                            100
-                          ).toFixed(0)}%`}
-                        </p>
-                      </li>
-                    )
-                )
-                .reverse()
-            : user &&
-              userSessions
-                .map(
-                  (userSession) =>
-                    user.displayName === userSession.displayName &&
-                    userSession.difficulty === level && (
-                      <li className="session-list">
-                        <p>Name: {userSession.displayName}</p>
-                        <p>Difficulty: {userSession.difficulty}</p>
-                        <p>Speed: {userSession.speed}</p>
-                        <p>Correct: {userSession.correct}</p>
-                        <p>
-                          Percent Correct:
-                          {`${(
-                            (userSession.correct / userSession.total) *
-                            100
-                          ).toFixed(0)}%`}
-                        </p>
-                      </li>
-                    )
-                )
-                .reverse()}
-        </ol>
+        level === "" || level === "Total" ? (
+          <>
+            <h2 className="game-history"> Total Game History</h2>
+            <div className="gamehistory-area">
+              <p>Total Questions: {userScore?.tT!} </p>
+              <p>Question Correct: {userScore?.tC!} </p>
+              <p>
+                Percentage Correct:{" "}
+                {`${((userScore?.tC! / userScore?.tT!) * 100).toFixed(0)}%`}{" "}
+              </p>
+            </div>
+          </>
+        ) : level === "Easy" ? (
+          <>
+            <h2 className="game-history"> Easy Game History</h2>
+            <div className="gamehistory-area">
+              <p>Total Questions: {userScore?.eT!} </p>
+              <p>Question Correct: {userScore?.eC!} </p>
+              <p>
+                Percentage Correct:{" "}
+                {`${((userScore?.eC! / userScore?.eT!) * 100).toFixed(0)}%`}
+              </p>
+            </div>
+          </>
+        ) : level === "Medium" ? (
+          <>
+            <h2 className="game-history"> Medium Game History</h2>
+            <div className="gamehistory-area">
+              <p>Total Questions: {userScore?.mT!} </p>
+              <p>Question Correct: {userScore?.mC!} </p>
+              <p>
+                Percentage Correct:{" "}
+                {`${((userScore?.mC! / userScore?.mT!) * 100).toFixed(0)}%`}{" "}
+              </p>
+            </div>
+          </>
+        ) : level === "Hard" ? (
+          <>
+            <h2 className="game-history"> Hard Game History</h2>
+            <div className="gamehistory-area">
+              <p>Total Questions: {userScore?.hT!} </p>
+              <p>Question Correct: {userScore?.hC!} </p>
+              <p>
+                Percentage Correct:{" "}
+                {`${((userScore?.hC! / userScore?.hT!) * 100).toFixed(0)}%`}
+              </p>
+            </div>
+          </>
+        ) : (
+          level === "Insanus" && (
+            <>
+              <h2 className="game-history"> Insanus Game History</h2>
+              <div className="gamehistory-area">
+                <p>Total Questions: {userScore?.iT!} </p>
+                <p>Question Correct: {userScore?.iC!} </p>
+                <p>
+                  Percentage Correct:{" "}
+                  {`${((userScore?.iC! / userScore?.iT!) * 100).toFixed(0)}%`}{" "}
+                </p>
+              </div>
+            </>
+          )
+        )
       ) : (
-        <h3>Sign In To See Your Session Info</h3>
+        <p>Sign In To See Your Game History</p>
       )}
+      <div className="session-area">
+        {level === "" ? (
+          <h2 className="sessions">Last 3 Sessions</h2>
+        ) : (
+          <h2 className="sessions">Sessions</h2>
+        )}
+
+        {user ? (
+          <ol className="orderedlist-container">
+            {level === ""
+              ? userSessions
+                  .map(
+                    (userSession) =>
+                      user.displayName === userSession.displayName && (
+                        <li className="session-list">
+                          <p>Name: {userSession.displayName}</p>
+                          <p>Difficulty: {userSession.difficulty}</p>
+                          <p>Speed: {userSession.speed}</p>
+                          <p>Correct: {userSession.correct}</p>
+
+                          <p>
+                            Percent Correct:
+                            {`${(
+                              (userSession.correct / userSession.total) *
+                              100
+                            ).toFixed(0)}%`}
+                          </p>
+                        </li>
+                      )
+                  )
+                  .slice(0, 3)
+                  .reverse()
+              : userSessions
+                  .map(
+                    (userSession) =>
+                      user.displayName === userSession.displayName &&
+                      (userSession.difficulty === level ||
+                        level === "Total") && (
+                        <li className="session-list">
+                          <p>Name: {userSession.displayName}</p>
+                          <p>Difficulty: {userSession.difficulty}</p>
+                          <p>Speed: {userSession.speed}</p>
+                          <p>Correct: {userSession.correct}</p>
+                          <p>
+                            Percent Correct:
+                            {`${(
+                              (userSession.correct / userSession.total) *
+                              100
+                            ).toFixed(0)}%`}
+                          </p>
+                        </li>
+                      )
+                  )
+                  .reverse()}
+          </ol>
+        ) : (
+          <h3>Sign In To See Your Session Info</h3>
+        )}
+      </div>
       {console.log(userSessions)}
     </div>
   );
