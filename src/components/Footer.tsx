19ce0d7b225f0c7200e515253add08c44d883770
Dusan <dmomcilo@hotmail.com>
2022-04-18 16:45:34 -0400

    statgraph almost

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,10 @@
 import { useEffect, useState } from "react";
-import DogFact from "../models/DogFact";
-import { getSingleDogFact } from "../services/DogServices";
 import "./Footer.css";
 
 const Footer = () => {
-  const [dogFact, setDogFact] = useState<DogFact>();
-
-  const getAndSetDogFact = () => {
-    getSingleDogFact().then((response) => {
-      setDogFact(response);
-    });
-  };
-
-  useEffect(() => {
-    getAndSetDogFact();
-  }, []);
-
   return (
     <footer className="Footer">
-      {console.log(dogFact)}
-
-      {<h3>{dogFact?.fact}</h3>}
+      <h2>23</h2>
     </footer>
   );
 };
